e83665fc8a8728738894900686c2d8ec608f93b0
Serhii_Makovetskyi <xxxsergeyxxx77@gmail.com>
2024-08-07 02:11:01 +0300

    BT_Enemy_Mage->Added decorator to check if target is dead

---
+++ Source/Shooter/BTD_IsHostileTargetDead.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BTD_IsHostileTargetDead.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "Character_Base.h"
+
+UBTD_IsHostileTargetDead::UBTD_IsHostileTargetDead()
+{
+	NodeName = "Is hostile target dead";
+}
+
+bool UBTD_IsHostileTargetDead::CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const
+{
+	UBlackboardComponent* BlackboardComp = OwnerComp.GetBlackboardComponent();
+	ACharacter_Base* HostileTargetActor = Cast<ACharacter_Base>(BlackboardComp->GetValueAsObject(HostileTarget.SelectedKeyName));
+
+	if (HostileTargetActor->IsDead())
+	{
+		return true;
+	}
+	return false;
+}
---
+++ Source/Shooter/BTD_IsHostileTargetDead.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTDecorator.h"
+#include "BTD_IsHostileTargetDead.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UBTD_IsHostileTargetDead : public UBTDecorator
+{
+	GENERATED_BODY()
+
+public:
+	UBTD_IsHostileTargetDead();
+
+protected:
+	virtual bool CalculateRawConditionValue(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) const override;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	FBlackboardKeySelector HostileTarget;
+
+};
